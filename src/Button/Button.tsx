b8c208a71e870579792c62289dd62db9858dc1dc
HankCode <henrik@naesmark.com>
2024-09-19 14:08:06 +0200

    Change hover state button

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -6,24 +6,20 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const buttonVariants = cva(
-  "px-[20px] flex flex-row items-center justify-center whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-gray-200 disabled:text-gray-400 hover:bg-yellow-100",
+  "px-[20px] flex flex-row items-center justify-center whitespace-nowrap rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:bg-gray-200 disabled:text-gray-400 hover:bg-yellow-200",
   {
     variants: {
       variant: {
         default: "bg-yellow-400 text-black  font-bold",
-        uppercase:
-          "uppercase bg-yellow-400 text-black font-normal tracking-wider",
-        disabled:
-          "bg-gray-200 text-gray-400 hover:bg-gray-200 pointer-events-none",
+        uppercase: "uppercase bg-yellow-400 text-black font-normal tracking-wider",
+        disabled: "bg-gray-200 text-gray-400 hover:bg-gray-200 pointer-events-none",
         tertiary:
           "bg-transparent text-black font-normal underline-offset-2 hover:underline-offset-4 hover:underline font-bold",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         outline: "border border-input bg-background font-bold",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
 
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 
         link: "font-bold bg-transparent text-black underline-offset-2 hover:underline-offset-4 hover:underline hover:bg-transparent px-0",
       },
@@ -104,9 +100,7 @@ export const LinkButton = React.forwardRef<HTMLAnchorElement, LinkButtonProps>(
         ref={ref}
         {...props}
       >
-        {isLoading && (
-          <FaSpinner className="animate-spin h-4 w-4 mr-4 text-black" />
-        )}
+        {isLoading && <FaSpinner className="animate-spin h-4 w-4 mr-4 text-black" />}
         {children}
         {rightIcon && <span className="ml-1">{rightIcon}</span>}
       </Link>
@@ -141,9 +135,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {isLoading && (
-          <FaSpinner className="animate-spin h-4 w-4 mr-4 text-black" />
-        )}
+        {isLoading && <FaSpinner className="animate-spin h-4 w-4 mr-4 text-black" />}
         {children}
         {rightIcon && <span className="ml-1">{rightIcon}</span>}
       </button>
